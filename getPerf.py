5f6b8ad6dc1ab75959e784914f88bc200be63513
Alan <alan.malta@cern.ch>
2013-08-06 15:35:30 +0200

    now the plots look OK with the worstOffender; next commit will be to implement RECO plots as well

--- getPerf.py
+++ getPerf.py
@@ -123,14 +123,13 @@ def makePlots(perf, worst):
     #print "\nworst: ", worst
     print "\nIterating over perf dict..."
     metrics = ['PeakValueVsize','PeakValueRss','AvgEventTime','TotalJobTime']
-    figCounter = 1
     # iterating over metrics
     for metric in metrics:
         print " ****** Metric: %s ******" % metric
         count = 0           # used to count the number of bars
-        pp.figure(figsize=(8, 12))
+        fig = pp.figure(figsize=(8, 12))
         xnames = []
-        values = []
+        xvalues = []
         yworst = []
         # iterating over workflows
         for wf,val1 in perf.iteritems():
@@ -148,7 +147,7 @@ def makePlots(perf, worst):
                     shortWf = shortWf[1:]
                     xnames.append(shortWf)
                     # it gets the mean of all values in the array
-                    values.append(np.mean(val2[metric]))
+                    xvalues.append(np.mean(val2[metric]))
                     # Gets the worst of the worstOffenders only
                     yworst.append(max(worst[wf][task][metric]))
                     print "Original worst: ", worst[wf][task][metric]
@@ -157,21 +156,21 @@ def makePlots(perf, worst):
             print "\nThe following graph will be made with the following values:\n"
             print "count: ", count
             print "xnames: ", xnames
-            print "values: ", values
+            print "xvalues: ", xvalues
             print "yworst: ", yworst
             pos = np.arange(count)+0.5        # the bar centers on the y axis
-            #bar(pos, values, yerr=rand(count), ecolor='r', align='center')
-            pp.bar(pos, values, yerr=yworst, ecolor='r', align='center')
+            #bar(pos, xvalues, yerr=rand(count), ecolor='r', align='center')
+            pp.bar(pos, xvalues, ecolor='r', align='center')
+            pp.plot(pos, yworst, 'rv', markersize=13)
             pp.xticks(pos, xnames, rotation=80)
             ### Tweaking the figure
-            pp.subplots_adjust(bottom=0.6)           # Automatically adjust subplot parameters to give specified padding
+            fig.subplots_adjust(bottom=0.6)           # Automatically adjust subplot parameters to give specified padding
             ### end tweaking
             pp.ylabel(metric)
             #xlabel('Workflow')
             pp.grid(True)
             filename = 'DIGI_'+metric+'.png'
-            pp.savefig('/afs/cern.ch/work/a/amaltaro/www/testPlots/'+filename)
-            figCounter += 1
+            fig.savefig('/afs/cern.ch/work/a/amaltaro/www/testPlots/'+filename)
         else:
             print "I have nothing to show for this DIGI task\n" 
 
