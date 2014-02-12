efcc1c57013e4864277d3c7ab682fd652920ccc4
Alan <alan.malta@cern.ch>
2014-02-12 14:09:33 +0100

    patch for deployment added; couple more user messages in the end

--- deployWMAgent.sh
+++ deployWMAgent.sh
@@ -3,6 +3,7 @@
 BASE_DIR=/data/srv 
 DEPLOY_DIR=$BASE_DIR/wmagent 
 ENV_FILE=/data/admin/wmagent/env.sh 
+CURRENT=/data/srv/wmagent/current
 MANAGE=/data/srv/wmagent/current/config/wmagent/ 
 PYTHON_WMA_DIR=$DEPLOY_DIR/v$WMA_TAG/sw.pre/$WMA_ARCH/cms/wmagent/$WMA_TAG/lib/python2.6/site-packages 
 
@@ -18,7 +19,6 @@ mkdir -p $DEPLOY_DIR || true
 
 cd $BASE_DIR
 rm -rf deployment;
-#git clone git://github.com/dmwm/deployment.git && cd deployment && git reset --hard $CMSWEB_TAG
 wget -O deployment.zip --no-check-certificate https://github.com/dmwm/deployment/archive/$CMSWEB_TAG.zip;
 unzip -q deployment.zip && cd deployment-$CMSWEB_TAG
 
@@ -51,17 +51,18 @@ echo "*** Starting services ***"
 echo "Done!" && echo
 sleep 5
 
-echo "*** Initializing the agent ***"
-./manage init-agent
-echo "Done!" && echo
-sleep 5
-
 ###
 # TODO: You can apply patches here
 ###
-# echo "*** Applying patches ***"
-# patch -d $PYTHON_WMA_DIR -p 3 < /data/cmsprod/dbs3_T0_patch1.patch
-###
+echo "*** Applying patches ***"
+cd $CURRENT
+wget https://github.com/dmwm/WMCore/pull/4954.patch -O - | patch -d sw/slc5_amd64_gcc461/cms/wmagent/$WMA_TAG/ -p 1 # for deployment
+
+echo "*** Initializing the agent ***"
+cd $MANAGE
+./manage init-agent
+echo "Done!" && echo
+sleep 5
 
 ###
 # tweak configuration
@@ -74,16 +75,11 @@ sed -i "s+config.PhEDExInjector.diskSites = \[\]+config.PhEDExInjector.diskSites
 sed -i "s+'Running': 169200, 'Pending': 360000, 'Error': 1800+'Running': 169200, 'Pending': 259200, 'Error': 1800+" $MANAGE/config.py
 echo "Done!" && echo
 
-#sed -i "s+LsfPluginJobGroup = '/groups/tier0/wmagent_testing'+LsfPluginJobGroup = '/groups/tier0/hufnagel/vocms104'+" ./config/tier0/config.py
-#sed -i "s+LsfPluginBatchOutput = 'None'+LsfPluginBatchOutput = '/afs/cern.ch/user/h/hufnagel/scratch0/tier0_logs'+" ./config/tier0/config.py
-#sed -i "s+cmsrepack+cmst0+g" ./config/tier0/config.py
-
 ###
 # set resource thresholds
 ###
 echo "*** Populating resource-control ***"
 cd $MANAGE
-# ./manage execute-agent wmagent-resource-control --add-all-sites  --plugin=CondorPlugin
 ./manage execute-agent wmagent-resource-control --add-all-sites  --plugin=CondorPlugin --pending-slots=50 --running-slots=50
 #./manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --cms-name=T2_CH_CERN --se-name=srm-eoscms.cern.ch --ce-name=T2_CH_CERN --pending-slots=1500 --running-slots=4000 --plugin=CondorPlugin
 #./manage execute-agent wmagent-resource-control --site-name=T2_CH_CERN --task-type=Processing --pending-slots=1500 --running-slots=4000
@@ -103,4 +99,7 @@ cd $MANAGE
 #./manage execute-agent wmagent-resource-control --site-name=T1_US_FNAL --task-type=LogCollect --pending-slots=100 --running-slots=100
 #./manage execute-agent wmagent-resource-control --site-name=T1_US_FNAL --task-type=Skim --pending-slots=100 --running-slots=100
 
-echo && echo "Deployment finished! Please take a look at the configuration and start-agent " && echo
+echo && echo "Deployment finished!! However you still need to:"
+echo "  1) Double check the configuration file (config.py)"
+echo "  2) Start the agent by: ./manage start-agent"
+echo && echo "Have a nice day!" && echo
