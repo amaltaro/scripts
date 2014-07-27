a36cfaaa53ce009828e3cda5a0ddc9e69f93a0d4
Alan <alan.malta@cern.ch>
2014-07-27 12:09:26 +0200

    Script to fetch workloadsummary and print/plot performance information

---
+++ WorkloadMon.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""
+WorkloadMon.py: fetch the workloadsummary from couch and make performance plots.
+
+This script is meant to parse all the performance fields present in a workload
+summary (stored in couchdb) and then nicely plot the metrics you are interested
+in.
+
+General development plan:
+ 1) code its UI: input via CLI and output in text mode only
+ 2) code its GUI: input via CLI and output in graphics (accessible via web)
+ 3) code its server side: input and output via webserver
+"""
+import sys, os
+import httplib, json
+import pprint
+from optparse import OptionParser
+
+#import sys,urllib,urllib2,re,time,os
+#import json
+#import optparse
+#import httplib
+#import datetime
+#import itertools
+#import matplotlib
+#matplotlib.use('agg')
+#import matplotlib.pyplot as pp
+#import numpy as np
+# metrics = ['PeakValueVsize', 'AvgEventTime', 'TotalJobTime', 'PeakValueRss']
+
+cmsweb_url = 'cmsweb-testbed.cern.ch'
+
+def getWorkloadSummary(request):
+    """
+    Receives the requestName as input and then fetch its workloadsummary
+    in couchdb. Returns a json object.
+    """
+    conn = httplib.HTTPSConnection(cmsweb_url, \
+           cert_file = os.getenv('X509_USER_PROXY'), \
+           key_file = os.getenv('X509_USER_PROXY'))
+    r1 = conn.request('GET', '/couchdb/workloadsummary/' + request)
+    r2 = conn.getresponse()
+    data = r2.read()
+    s = json.loads(data)
+    conn.close()
+    #except:
+    #   print "Cannot get request (getWorkloadSummary) " 
+    #   sys.exit(1)
+    print "\nGot it!"
+    return s
+
+def main():
+    """
+    First you need to source cms_ui env, so you get your X509_USER_PROXY exported.
+    Then you provide a requestName that will be fetched from couchdb in order to
+    be mined and have its performance information nicely shown.
+    """
+    usage = "Usage: %prog -r requestName"
+    parser = OptionParser(usage = usage)
+    parser.add_option('-r', '--request', help = 'Request name', dest = 'request')
+    (options, args) = parser.parse_args()
+    if not options.request:
+        parser.error('You must provide a request name')
+        sys.exit(1)
+
+    request = options.request
+    reqout = getWorkloadSummary(request)
+    pprint.pprint(reqout)
+    print "Work done!"
+    sys.exit(0)
+
+if __name__ == "__main__":
+        main()
