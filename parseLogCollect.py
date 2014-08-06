e10b7b0992296b01a1a042eef56a768ece5c9bea
Alan <alan.malta@cern.ch>
2014-08-06 17:12:47 +0200

    filtering out the wanted metrics only

--- parseLogCollect.py
+++ parseLogCollect.py
@@ -2,7 +2,7 @@
 import os, sys
 import subprocess
 from optparse import OptionParser
-
+from xml.dom import minidom
 
 def main():
     """
@@ -23,11 +23,26 @@ def main():
     out, err = p.communicate()
     tarballs = out.split()
 
+    readMetrics = ["Timing-file-read-maxMsecs","Timing-tstoragefile-read-maxMsecs","Timing-tstoragefile-readActual-maxMsecs"
+                   "Timing-file-read-numOperations","Timing-tstoragefile-read-numOperations","Timing-tstoragefile-readActual-numOperations",
+                   "Timing-file-read-totalMegabytes","Timing-tstoragefile-read-totalMegabytes","Timing-tstoragefile-readActual-totalMegabytes",
+                   "Timing-file-read-totalMsecs","Timing-tstoragefile-read-totalMsecs","Timing-tstoragefile-readActual-totalMsecs"]
+
+    writeMetrics = ["Timing-file-write-maxMsecs","Timing-tstoragefile-write-maxMsecs","Timing-tstoragefile-writeActual-maxMsecs"
+                   "Timing-file-write-numOperations","Timing-tstoragefile-write-numOperations","Timing-tstoragefile-writeActual-numOperations",
+                   "Timing-file-write-totalMegabytes","Timing-tstoragefile-write-totalMegabytes","Timing-tstoragefile-writeActual-totalMegabytes",
+                   "Timing-file-write-totalMsecs","Timing-tstoragefile-write-totalMsecs","Timing-tstoragefile-writeActual-totalMsecs"]
+
     for file in tarballs:
         subcommand = ["tar", "-x", "cmsRun1/FrameworkJobReport.xml", "-zvf", file]
         print "Subcommand: %s" % subcommand
 
-        p = subprocess.Popen(subcommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.call(subcommand)
+        xmldoc = minidom.parse("cmsRun1/FrameworkJobReport.xml")
+        items = ( (item.getAttribute('Name'),item.getAttribute('Value')) for item in xmldoc.getElementsByTagName('Metric') )
+        #matching = [item for item in items if item[0] in readMetrics or item[0] in writeMetrics ]
+        matching = [item for item in items if item[0] in readMetrics ]
+        print matching
 
     sys.exit(0)
 
