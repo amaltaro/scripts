e03b083bc1b2dbabc14f097f2b42630e44ed7fb2
Alan <alan.malta@cern.ch>
2014-08-11 19:36:28 +0200

    now it acceps a file with a list of tarballs

--- parseLogCollect.py
+++ parseLogCollect.py
@@ -13,12 +13,13 @@ def main():
     usage = "Usage: %prog -l logCollect"
     parser = OptionParser(usage = usage)
     parser.add_option('-l', '--logCollet', help = 'Tarball for the logCollect jobs', dest = 'logCol')
-    parser.add_option('-f', '--fileOut', help = 'Output file containing info in json format', dest = 'fileOut')
+    parser.add_option('-i', '--inputFile', help = 'Input file containing the logCollect tarball names', dest = 'input')
+    parser.add_option('-o', '--outputFile', help = 'Output file containing info in json format', dest = 'output')
     parser.add_option('-s', '--short', action = "store_true", 
                       help = 'Use it for short summary (8 metrics instead of 24)', dest = 'short')
     (options, args) = parser.parse_args()
-    if not options.logCol:
-        parser.error('You must provide a logCollect tarball')
+    if not options.logCol and not options.input:
+        parser.error('You must either provide a logCollect tarball or a file with their names')
         sys.exit(1)
     if options.short:
         readMetrics = ["Timing-file-read-maxMsecs","Timing-file-read-numOperations",
@@ -39,31 +40,37 @@ def main():
                         "Timing-tstoragefile-writeActual-totalMegabytes","Timing-file-write-totalMsecs",
                         "Timing-tstoragefile-write-totalMsecs","Timing-tstoragefile-writeActual-totalMsecs"]
 
-    command = ["tar", "xvf", options.logCol]
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    tarballs = out.split()
+    if options.logCol:
+        logCollects = [options.logCol]
+    elif options.input:
+        logCollects = []
+        f = open(options.input, 'r')
+        for tar in f:
+            tar = tar.rstrip('\n')
+            logCollects.append(tar)
 
     total = {}
-    for i, file in enumerate(tarballs):
-        subcommand = ["tar", "-x", "cmsRun1/FrameworkJobReport.xml", "-zvf", file]
+    numtarballs = 0
+    for tarball in logCollects:
+        command = ["tar", "xvf", tarball]
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        tarballs = out.split()
+        for file in tarballs:
+            subcommand = ["tar", "-x", "cmsRun1/FrameworkJobReport.xml", "-zvf", file]
 
-        # making this call quiet, so /dev/null
-        subprocess.call(subcommand, stdout=open(os.devnull, 'wb'))
-        xmldoc = minidom.parse("cmsRun1/FrameworkJobReport.xml")
-        items = ( (item.getAttribute('Name'),item.getAttribute('Value')) for item in xmldoc.getElementsByTagName('Metric') )
-        matched = [item for item in items if item[0] in readMetrics or item[0] in writeMetrics ]
-        #matched = [item for item in items if item[0] in readMetrics ]
-        for ele in matched:
-            if not i:
-                total[ele[0]] = [float(ele[1])]
-            else:
-                total[ele[0]].append(float(ele[1]))
-
-    if options.fileOut:
-        with open(options.fileOut, 'w') as outFile:
-            json.dump(total, outFile)
-            outFile.close()
+            # making this call quiet, so /dev/null
+            subprocess.call(subcommand, stdout=open(os.devnull, 'wb'))
+            xmldoc = minidom.parse("cmsRun1/FrameworkJobReport.xml")
+            items = ( (item.getAttribute('Name'),item.getAttribute('Value')) for item in xmldoc.getElementsByTagName('Metric') )
+            matched = [item for item in items if item[0] in readMetrics or item[0] in writeMetrics ]
+            #matched = [item for item in items if item[0] in readMetrics ]
+            for ele in matched:
+                if not numtarballs:
+                    total[ele[0]] = [float(ele[1])]
+                else:
+                    total[ele[0]].append(float(ele[1]))
+            numtarballs += 1
 
     # Calculates average, standard deviation (old way, no numpy available) and
     # finds the maximum and minimum values
@@ -91,6 +98,11 @@ def main():
     print ""
     for metric in writeMetrics: 
         print "%-47s : %s" % (metric, results[metric])
+
+    if options.output:
+        with open(options.output, 'w') as outFile:
+            json.dump(total, outFile)
+            outFile.close()
 
     sys.exit(0)
 
