39c3c2cd550da2df8656e8ccfef0bc1a35ac1b19
Alan <alan.malta@cern.ch>
2014-09-11 12:37:12 +0200

    provide two data structure options, array or dict (dict needs to be implemented)

--- parseLogCollect.py
+++ parseLogCollect.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python -u
 import os, sys, json
 import subprocess
-import pprint
+from pprint import pprint
 from optparse import OptionParser
 from xml.dom import minidom
 from xml.parsers.expat import ExpatError
@@ -13,58 +13,11 @@ from numpy import mean, std
 ### TODO: I'm cleaning up the write metrics because it looks like it's completely unreliable
 ### TODO: read metrics are also not very reliable, but ... let's keep them a bit longer
 
-def main():
+def buildStrucOfArrays(logCollects, metrics, writeOut = None):
     """
-    Provide a logCollect tarball as input (in your local machine) or a text file
-    with their name.
-
-    export SCRAM_ARCH=slc5_amd64_gcc462
-    cd /build/relval/CMSSW_5_3_0/src/
-    cmsenv
+    It will create a dict of arrays where the key names are written only once, so
+    taking much less memory to store the whole structure
     """
-    usage = "Usage: %prog -l logCollect -i inputFile [-o outputFile] [--short]"
-    parser = OptionParser(usage = usage)
-    parser.add_option('-l', '--logCollet', help = 'Tarball for the logCollect jobs', dest = 'logCol')
-    parser.add_option('-i', '--inputFile', help = 'Input file containing the logCollect tarball names', dest = 'input')
-    parser.add_option('-o', '--outputFile', help = 'Output file containing info in json format', dest = 'output')
-    parser.add_option('-s', '--short', action = "store_true", 
-                      help = 'Use it for short summary (8 metrics instead of 24)', dest = 'short')
-    (options, args) = parser.parse_args()
-    if not options.logCol and not options.input:
-        parser.error('You must either provide a logCollect tarball or a file with their names')
-        sys.exit(1)
-    if options.short:
-#        metrics = ["Timing-file-read-maxMsecs","Timing-file-read-numOperations",
-#                   "Timing-file-read-totalMegabytes","Timing-file-read-totalMsecs",
-#                   "Timing-file-write-totalMegabytes","AvgEventTime","TotalJobTime","TotalJobCPU"]
-        metrics = ["Timing-tstoragefile-read-maxMsecs","Timing-tstoragefile-read-numOperations",
-                   "Timing-tstoragefile-read-totalMegabytes","Timing-tstoragefile-read-totalMsecs",
-                   "Timing-file-write-totalMegabytes","AvgEventTime","TotalJobTime","TotalJobCPU",
-                   "CPUModels","averageCoreSpeed","totalCPUs"]
-    else:
-        metrics = ["Timing-file-read-maxMsecs","Timing-tstoragefile-read-maxMsecs",
-                   "Timing-tstoragefile-readActual-maxMsecs","Timing-file-read-numOperations",
-                   "Timing-tstoragefile-read-numOperations","Timing-tstoragefile-readActual-numOperations",
-                   "Timing-file-read-totalMegabytes","Timing-tstoragefile-read-totalMegabytes",
-                   "Timing-tstoragefile-readActual-totalMegabytes","Timing-file-read-totalMsecs",
-                   "Timing-tstoragefile-read-totalMsecs","Timing-tstoragefile-readActual-totalMsecs",
-                   "Timing-file-write-maxMsecs","Timing-tstoragefile-write-maxMsecs",
-                   "Timing-tstoragefile-writeActual-maxMsecs","Timing-file-write-numOperations",
-                   "Timing-tstoragefile-write-numOperations","Timing-tstoragefile-writeActual-numOperations",
-                   "Timing-file-write-totalMegabytes","Timing-tstoragefile-write-totalMegabytes",
-                   "Timing-tstoragefile-writeActual-totalMegabytes","Timing-file-write-totalMsecs",
-                   "Timing-tstoragefile-write-totalMsecs","Timing-tstoragefile-writeActual-totalMsecs",
-                   "AvgEventTime", "TotalJobTime","CPUModels","averageCoreSpeed","totalCPUs"]
-
-    if options.logCol:
-        logCollects = [options.logCol]
-    elif options.input:
-        logCollects = []
-        f = open(options.input, 'r')
-        for tar in f:
-            tar = tar.rstrip('\n')
-            logCollects.append(tar)
-
     dictRun, results = [{}, {}], [{}, {}]
 
     for i, _ in enumerate(dictRun):
@@ -83,7 +36,7 @@ def main():
         for logArchive in logArchives:
             print logArchive
             # then uncompress each tarball inside the big logCollect
-            subcommand = ["tar", "-x", "cmsRun?/FrameworkJobReport.xml", "-zvf", logArchive] 
+            subcommand = ["tar", "-x", "cmsRun?/FrameworkJobReport.xml", "-zvf", logArchive]
             q = subprocess.Popen(subcommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = q.communicate()
             cmsRuns = sorted(out.split())
@@ -118,7 +71,7 @@ def main():
             results[j][k]['std'] = "%.3f" % std(v)
             results[j][k]['min'] = "%.3f" % min(v)
             results[j][k]['max'] = "%.3f" % max(v)
-    
+
     # Printing outside the upper for, so we can kind of order it...
     for i, step in enumerate(results):
         if not step:
@@ -127,16 +80,86 @@ def main():
         for metric in metrics: 
             print "%-47s : %s" % (metric, step[metric])
 
-    if options.output:
-        print ""
+    # Debug
+    #pprint(dictRun)
+                   
+    if writeOut:
+        print ""   
         for i, step in enumerate(dictRun):
             if not step['TotalJobTime']:
                 continue
-            filename = 'cmsRun' + str(i+1) + '_' + options.output
+            filename = 'cmsRun' + str(i+1) + '_' + writeOut
             print "Dumping whole cmsRun%d json into %s" % (i+1, filename)
             with open(filename, 'w') as outFile:
                 json.dump(step, outFile)
                 outFile.close()
+    return
+
+def main():
+    """
+    Provide a logCollect tarball as input (in your local machine) or a text file
+    with their name.
+
+    export SCRAM_ARCH=slc5_amd64_gcc462
+    cd /build/relval/CMSSW_5_3_0/src/
+    cmsenv
+    """
+    usage = "Usage: %prog -t tarball -i inputFile [-o outputFile] [--long] [--array] [--dic]"
+    parser = OptionParser(usage = usage)
+    parser.add_option('-t', '--tarball', help = 'Tarball for the logCollect jobs', dest = 'tar')
+    parser.add_option('-i', '--inputFile', help = 'Input file containing the logCollect tarball names', dest = 'input')
+    parser.add_option('-o', '--outputFile', help = 'Output file containing info in json format', dest = 'output')
+    parser.add_option('-l', '--long', action = "store_true", 
+                      help = 'Use it to make a long summary (27 metrics in total)', dest = 'long')
+    parser.add_option('-a', '--array', action = "store_true", help = 'Produces a structure of arrays', dest = 'array')
+    parser.add_option('-d', '--dic', action = "store_true", help = 'Produces an array of dictionaries', dest = 'dict')
+    (options, args) = parser.parse_args()
+    if not options.tar and not options.input:
+        parser.error('You must either provide a logCollect tarball or a file with their names')
+        sys.exit(1)
+    if not options.array and not options.dict:
+        parser.error('You must choose which data structure you want to build')
+        sys.exit(1)
+
+    if options.long:
+        metrics = ["Timing-file-read-maxMsecs","Timing-tstoragefile-read-maxMsecs",
+                   "Timing-tstoragefile-readActual-maxMsecs","Timing-file-read-numOperations",
+                   "Timing-tstoragefile-read-numOperations","Timing-tstoragefile-readActual-numOperations",
+                   "Timing-file-read-totalMegabytes","Timing-tstoragefile-read-totalMegabytes",
+                   "Timing-tstoragefile-readActual-totalMegabytes","Timing-file-read-totalMsecs",
+                   "Timing-tstoragefile-read-totalMsecs","Timing-tstoragefile-readActual-totalMsecs",
+                   "Timing-file-write-maxMsecs","Timing-tstoragefile-write-maxMsecs",
+                   "Timing-tstoragefile-writeActual-maxMsecs","Timing-file-write-numOperations",
+                   "Timing-tstoragefile-write-numOperations","Timing-tstoragefile-writeActual-numOperations",
+                   "Timing-file-write-totalMegabytes","Timing-tstoragefile-write-totalMegabytes",
+                   "Timing-tstoragefile-writeActual-totalMegabytes","Timing-file-write-totalMsecs",
+                   "Timing-tstoragefile-write-totalMsecs","Timing-tstoragefile-writeActual-totalMsecs",
+                   "AvgEventTime", "TotalJobTime","CPUModels","averageCoreSpeed","totalCPUs"]
+    else:
+#        metrics = ["Timing-file-read-maxMsecs","Timing-file-read-numOperations",
+#                   "Timing-file-read-totalMegabytes","Timing-file-read-totalMsecs",
+#                   "Timing-file-write-totalMegabytes","AvgEventTime","TotalJobTime","TotalJobCPU"]
+        metrics = ["Timing-tstoragefile-read-maxMsecs","Timing-tstoragefile-read-numOperations",
+                   "Timing-tstoragefile-read-totalMegabytes","Timing-tstoragefile-read-totalMsecs",
+                   "Timing-file-write-totalMegabytes","AvgEventTime","TotalJobTime","TotalJobCPU",
+                   "CPUModels","averageCoreSpeed","totalCPUs"]
+
+    if options.tar:
+        logCollects = [options.tar]
+    elif options.input:
+        logCollects = []
+        f = open(options.input, 'r')
+        for tar in f:
+            tar = tar.rstrip('\n')
+            logCollects.append(tar)
+
+    if options.array:
+        # the array index defines a job (dict1 is cmsRun1, dict2 is cmsRun2) 
+        # e.g. [{'AvgEventTime': [40.41, 37.4, 46.8], 'TotalJobCPU': [7772.4, 7764.5, 8349.0], etc}]
+        buildStrucOfArrays(logCollects, metrics, options.output)
+    elif options.dict:
+        print "NOT_IMPLEMENTED_YET"
+        sys.exit(1)
 
     sys.exit(0)
 
