0dbf44ffe391241a95ded56a93c2e638a47e35b3
Alan Malta Rodrigues <amaltaro@uscmsmac02.dyndns.cern.ch>
2017-01-09 12:01:41 +0100

    Improve the creation dict

--- ReqMgr2/buildWMCoreTemplates.py
+++ ReqMgr2/buildWMCoreTemplates.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+"""
+Run it from vocms049 with your proxy in the environment
+"""
+from __future__ import print_function
+
 import sys
 import os
 import random
 import json
 import httplib
-from pprint import pprint
 
 
 def getRequestDict(workflow):
@@ -25,83 +29,112 @@ def updateRequestDict(reqDict):
     Remove some keys from the original dict and build the
     structure expected by the reqmgr client script.
     """
-    paramBlacklist = ['_id', 'AllowOpportunistic', 'BlockCloseMaxEvents', 'BlockCloseMaxFiles', 'BlockCloseMaxSize',
-                      'BlockCloseMaxWaitTime', 'CouchURL', 'CouchWorkloadDBName', 'CustodialGroup', 'CustodialSubType',
-                      'Dashboard', 'DeleteFromSource', 'GracePeriod', 'HardTimeout', 'InitialPriority', 'InputDatasets',
-                      'inputMode', 'MaxMergeEvents', 'MaxMergeSize', 'MaxRSS', 'MaxVSize', 'MinMergeSize',
-                      'NonCustodialGroup', 'NonCustodialSubType', 'OutputDatasets', 'ReqMgr2Only', 'RequestDate',
-                      'RequestSizeFiles', 'RequestorDN', 'RequestName', 'RequestStatus', 'RequestTransition',
-                      'RequestWorkflow', 'SiteWhitelist', 'SoftTimeout', 'SoftwareVersions', 'SubscriptionPriority',
-                      'Team', 'Teams', 'timeStamp', 'TrustSitelists', 'TrustPUSitelists', 'TotalEstimatedJobs',
-                      'TotalInputEvents', 'TotalInputLumis', 'TotalInputFiles']
+    paramBlacklist = ['AllowOpportunistic', 'AutoApproveSubscriptionSites', 'BlockCloseMaxEvents', 'BlockCloseMaxFiles', 'BlockCloseMaxSize',
+                      'BlockCloseMaxWaitTime', 'CouchURL', 'CouchWorkloadDBName', 'CustodialGroup', 'CustodialSites', 'CustodialSubType',
+                      'Dashboard', 'DeleteFromSource', 'GracePeriod', 'Group', 'HardTimeout', 'InitialPriority', 'InputDatasets',
+                      'MaxMergeEvents', 'MaxMergeSize', 'MaxVSize', 'MergedLFNBase', 'MinMergeSize',
+                      'NonCustodialGroup', 'NonCustodialSites', 'NonCustodialSubType', 'OutputDatasets', 'ReqMgr2Only', 'RequestDate',
+                      'RequestName', 'RequestSizeFiles', 'RequestStatus', 'RequestTransition', 'RequestWorkflow',
+                      'RequestorDN', 'SiteWhitelist', 'SoftTimeout', 'SoftwareVersions', 'SubscriptionPriority',
+                      'Team', 'Teams', 'TotalEstimatedJobs', 'TotalInputEvents', 'TotalInputFiles', 'TotalInputLumis', 'TotalTime',
+                      'TrustPUSitelists', 'TrustSitelists', 'UnmergedLFNBase', '_id', 'inputMode', 'timeStamp',
+                      'DN', 'DQMHarvestUnit', 'DashboardHost', 'DashboardPort', 'EnableNewStageout', 'FirstEvent',
+                      'FirstLumi', 'PeriodicHarvestInterval', 'RobustMerge', 'RunNumber', 'ValidStatus', 'VoGroup',
+                      'VoRole', 'dashboardActivity', 'mergedLFNBase', 'unmergedLFNBase', 'MaxWaitTime']
 
-    newSchema = {}
+
+    createDict = {}
     for key, value in reqDict.items():
         if key in paramBlacklist or value in ([], {}, None, ''):
             continue
         elif key == 'OpenRunningTimeout':
             continue
         elif key == 'Campaign':
-            newSchema[key] = "Campaign-OVERRIDE-ME"
+            createDict[key] = "Campaign-OVERRIDE-ME"
         elif key == 'RequestString':
-            newSchema[key] = "RequestString-OVERRIDE-ME"
+            createDict[key] = "RequestString-OVERRIDE-ME"
         elif key == 'DQMUploadUrl':
-            newSchema[key] = "https://cmsweb-testbed.cern.ch/dqm/dev;https://cmsweb.cern.ch/dqm/relval-test"
+            createDict[key] = "https://cmsweb-testbed.cern.ch/dqm/dev"
         elif key == 'DbsUrl':
-            newSchema[key] = "https://cmsweb-testbed.cern.ch/dbs/int/global/DBSReader/"
-        elif key == 'MergedLFNBase':
-            newSchema['MergedLFNBase'] = "/store/backfill/1"
+            createDict[key] = "https://cmsweb-testbed.cern.ch/dbs/int/global/DBSReader/"
         elif key == 'RequestPriority':
-            newSchema[key] = min(value + 100000, 999999)
+            createDict[key] = min(value + 100000, 999999)
         elif key == 'PrepID':
-            newSchema[key] = 'TEST-' + value
+            createDict[key] = 'TEST-' + value
+        elif key in ['ConfigCacheURL', 'ConfigCacheUrl']:
+            createDict['ConfigCacheUrl'] = value
         else:
-            newSchema[key] = value
+            createDict[key] = value
 
-    newSchema['Comments'] = ""
-    newSchema['Requestor'] = "amaltaro"
-    newSchema['Group'] = "DATAOPS"
-    newSchema = {'createRequest': newSchema}
-
-    # add assignment parameters
-    _addAssignmentParams(newSchema)
+    createDict['Comments'] = ""
+    createDict['Requestor'] = "amaltaro"
+    newSchema = {'createRequest': createDict}
+    if createDict['RequestType'] in ['TaskChain', 'StepChain']:
+        handleTasksSteps(createDict)
+    newSchema['assignRequest'] = handleAssignmentParams(createDict)
 
     return newSchema
 
+def handleTasksSteps(reqDict):
+    """
+    Remove/overwrite some values
+    """
+    if 'TaskChain' in reqDict:
+      name = 'Task'
+      number = reqDict['TaskChain']
+    else:
+      name = 'Step'
+      number = reqDict['StepChain']
+    for i in range(1, number + 1):
+        thisDict = name + str(i)
+        for k in reqDict[thisDict].keys():
+            # remove empty stuff
+            if not reqDict[thisDict][k]:
+                reqDict[thisDict].pop(k)
+            elif k == 'ProcessingString':
+                reqDict[thisDict][k] = "%s%s_WMCore_TEST" % (name, i)
 
-def _addAssignmentParams(reqDict):
+
+def handleAssignmentParams(reqDict):
     """
     Add some predefined assignment parameters to the template
     """
-    reqDict['assignRequest'] = {"SiteWhitelist": ["SiteWhitelist-OVERRIDE-ME"],
-                                "Team": "Team-OVERRIDE-ME",
-                                "AcquisitionEra": "AcquisitionEra-OVERRIDE-ME",
-                                "ProcessingString": "ProcessingString-OVERRIDE-ME",
-                                "Dashboard": "Dashboard-OVERRIDE-ME",
-                                "ProcessingVersion": 9,
-                                "MergedLFNBase": "/store/backfill/1",
-                                "UnmergedLFNBase": "/store/unmerged",
-                                "MaxRSS": reqDict['createRequest']['Memory'] * 1000,
-                                "MaxVSize": reqDict['createRequest'].get('Multicore', 1) * 20000000,
-                                "SoftTimeout": 129600,
-                                "GracePeriod": 300,
-                                "SiteBlacklist": [],
-                                #                                "TrustSitelists": False,
-                                #                                "TrustPUSitelists": False,
-                                #                                "MinMergeSize": 2147483648,
-                                #                                "MaxMergeSize": 4294967296,
-                                #                                "MaxMergeEvents": 50000,
-                                #                                "CustodialSites": [],
-                                #                                "NonCustodialSites": [],
-                                #                                "AutoApproveSubscriptionSites": [],
-                                #                                "SubscriptionPriority": "Low",
-                                #                                "CustodialSubType": "Move",
-                                #                                "BlockCloseMaxWaitTime": 14400,
-                                #                                "BlockCloseMaxFiles": 500,
-                                #                                "BlockCloseMaxEvents": 200000000,
-                                #                                "BlockCloseMaxSize": 5000000000000
-                                }
-    return
+    assignDict = {"SiteWhitelist": ["SiteWhitelist-OVERRIDE-ME"],
+                  "Team": "Team-OVERRIDE-ME",
+                  "AcquisitionEra": "AcquisitionEra-OVERRIDE-ME",
+                  "ProcessingString": "ProcessingString-OVERRIDE-ME",
+                  "Dashboard": "Dashboard-OVERRIDE-ME",
+                  "ProcessingVersion": 19,
+                  "MergedLFNBase": "/store/backfill/1",
+                  "UnmergedLFNBase": "/store/unmerged",
+                  "MaxRSS": reqDict.pop('MaxRSS'),
+                  "MaxVSize": 40000000,
+                  "SoftTimeout": 129600,
+                  "GracePeriod": 300,
+                  "SiteBlacklist": [],
+                  #                                "TrustSitelists": False,
+                  #                                "TrustPUSitelists": False,
+                  #                                "MinMergeSize": 2147483648,
+                  #                                "MaxMergeSize": 4294967296,
+                  #                                "MaxMergeEvents": 50000,
+                  #                                "CustodialSites": [],
+                  #                                "NonCustodialSites": [],
+                  #                                "AutoApproveSubscriptionSites": [],
+                  #                                "SubscriptionPriority": "Low",
+                  #                                "CustodialSubType": "Move",
+                  #                                "BlockCloseMaxWaitTime": 14400,
+                  #                                "BlockCloseMaxFiles": 500,
+                  #                                "BlockCloseMaxEvents": 200000000,
+                  #                                "BlockCloseMaxSize": 5000000000000
+                 }
+
+    for k in ['AcquisitionEra', 'ProcessingString']:
+        if isinstance(reqDict[k], dict):
+            assignDict[k] = {}
+            for task, _ in reqDict[k].iteritems():
+                assignDict[k][task] = k + "-OVERRIDE-ME"
+
+    return assignDict
 
 
 def createJsonTemplate(reqDict):
