15caf1778cd05a3f54ff2156a87e82d8e5aa7ccd
amaltaro <alan.malta@cern.ch>
2017-05-04 12:02:50 +0200

    Read input request and create a DQMHarvest one, based on reqmgr2

--- injectHarvest.py
+++ injectHarvest.py
@@ -6,37 +6,35 @@ Pre-requisites:
 
 import sys
 import os
-import re
-import shlex
 import json
-import urllib
 import httplib
-from optparse import OptionParser
-from subprocess import call
-from time import sleep
+from copy import copy
 from pprint import pprint
-from WMCore.WMSpec.WMWorkload import WMWorkloadHelper
 
 url = "cmsweb.cern.ch"
-reqmgrCouchURL = "https://"+url+"/couchdb/reqmgr_workload_cache"
+reqmgrCouchURL = "https://" + url + "/couchdb/reqmgr_workload_cache"
 
-DEFAULT_DICT = {#'CouchURL': 'https://cmsweb.cern.ch/couchdb',
-                'ConfigCacheUrl': "https://cmsweb.cern.ch/couchdb",
-                'DbsUrl': 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader',
-                'Group': 'DATAOPS',
-                'Memory': 2200,
-                'OpenRunningTimeout': 0,
-                'RequestPriority': 140000,
-                'RequestType': 'DQMHarvest',
-                'Requestor': 'amaltaro',
-                'mergedLFNBase': '/store/data',
-                'unmergedLFNBase': '/store/unmerged'}
-
-UPDATE_KEYS = ['InputDataset', 'Scenario', 'ProcessingVersion',
-               'ProcessingString', 'ScramArch', 'SizePerEvent',
-               'DQMConfigCacheID', 'RequestString', 'DQMUploadUrl',
-               'GlobalTag', 'Campaign', 'TimePerEvent', 'AcquisitionEra',
-               'CMSSWVersion']
+DEFAULT_DICT = {
+    "AcquisitionEra": "UPDATEME",
+    "CMSSWVersion": "UPDATEME",
+    "Campaign": "UPDATEME",
+    "ConfigCacheUrl": "https://cmsweb.cern.ch/couchdb",
+    "DQMConfigCacheID": "UPDATEME",
+    "DQMHarvestUnit": "UPDATEME",
+    "DQMUploadUrl": "UPDATEME",
+    "DbsUrl": "https://cmsweb.cern.ch/dbs/prod/global/DBSReader/",
+    "GlobalTag": "UPDATEME",
+    "InputDataset": "UPDATEME",
+    "Memory": 2200,
+    "PrepID": "UPDATEME",
+    "ProcessingString": "UPDATEME",
+    "ProcessingVersion": 1,
+    "RequestPriority": 999999,
+    "RequestString": "UPDATEME",
+    "RequestType": "DQMHarvest",
+    "ScramArch": "UPDATEME",
+    "SizePerEvent": 1600,
+    "TimePerEvent": 1}
 
 
 def main():
@@ -48,96 +46,81 @@ def main():
     newDict = buildRequest(work)
     pprint(newDict)
     workflow = submitWorkflow(newDict)
-    print workflow
     approveRequest(workflow)
     sys.exit(0)
 
 
 def retrieveWorkload(workflowName):
-    conn = httplib.HTTPSConnection(url, 
-                                   cert_file = os.getenv('X509_USER_PROXY'),
-                                   key_file = os.getenv('X509_USER_PROXY'))
-    r1 = conn.request('GET', '/couchdb/reqmgr_workload_cache/' + workflowName)
+    headers = {"Content-type": "application/json",
+               "Accept": "application/json"}
+    conn = httplib.HTTPSConnection(url, cert_file=os.getenv('X509_USER_PROXY'), key_file=os.getenv('X509_USER_PROXY'))
+    urn = "/reqmgr2/data/request/%s" % workflowName
+    conn.request("GET", urn, headers=headers)
     r2 = conn.getresponse()
-    data = r2.read()
-    conn.close()
-    s = json.loads(data)
-    #pprint(s)
-    return s
+    request = json.loads(r2.read())["result"][0][workflowName]
+    return request
 
 
 def buildRequest(req_cache):
-    newSchema = DEFAULT_DICT
-    for k in UPDATE_KEYS:
-        if k == 'RequestString':
-            newSchema[k] = req_cache[k] + '_Harv'
-        elif k == 'InputDataset':
-            dset = [d for d in req_cache['OutputDatasets'] if d.endswith(tuple(['/DQM', '/DQMIO']))]
-            newSchema[k] = dset[0]
-        elif k == 'ProcessingVersion':
-            newSchema[k] = int(req_cache[k])
+    newSchema = copy(DEFAULT_DICT)
+    for k, v in DEFAULT_DICT.iteritems():
+        if v != "UPDATEME":
+            pass
         else:
-            newSchema[k] = req_cache[k]
-        if isinstance(newSchema[k], basestring):
-          newSchema[k] = str(newSchema[k])
-        else:
-          newSchema[k] = int(newSchema[k])
-    #pprint(newSchema)
+            if k == 'RequestString':
+                newSchema[k] = req_cache[k] + '_Harv'
+            elif k == 'InputDataset':
+                dset = [d for d in req_cache['OutputDatasets'] if d.endswith(tuple(['/DQM', '/DQMIO']))]
+                newSchema[k] = dset[0]
+            else:
+                if isinstance(req_cache[k], dict):
+                    # then simply pick the first value, makes no difference in the end
+                    newSchema[k] = req_cache[k].values()[0]
+                else:
+                    newSchema[k] = req_cache[k]
+
     return newSchema
 
 
 def submitWorkflow(schema):
-    for schemaListItem in ["RunWhitelist", "RunBlacklist", "BlockWhitelist", "BlockBlacklist"]:
-        if schemaListItem in schema.keys():
-            schema[schemaListItem] = str(schema[schemaListItem])
-
-    jsonEncodedParams = {}
-    for paramKey in schema.keys():
-        jsonEncodedParams[paramKey] = json.dumps(schema[paramKey])
-
-    encodedParams = urllib.urlencode(jsonEncodedParams, False)
-
-    headers = {"Content-type": "application/x-www-form-urlencoded",
-                 "Accept": "text/plain"}
-
-    conn = httplib.HTTPSConnection(url, cert_file = os.getenv('X509_USER_PROXY'), key_file = os.getenv('X509_USER_PROXY'))
-    print "\nsubmitting new workflow to %s" % url
-    conn.request("POST",  "/reqmgr/create/makeSchema", encodedParams, headers)
-    response = conn.getresponse()
-    #print response.status, response.reason
-    data = response.read()
-    #print data
-    details=re.search("details\/(.*)\'",data)
-    return details.group(1)
+    headers = {"Content-type": "application/json",
+               "Accept": "application/json"}
+    encodedParams = json.dumps(schema)
+    conn = httplib.HTTPSConnection(url, cert_file=os.getenv('X509_USER_PROXY'), key_file=os.getenv('X509_USER_PROXY'))
+    print "Submitting new workflow..."
+    conn.request("POST", "/reqmgr2/data/request", encodedParams, headers)
+    resp = conn.getresponse()
+    data = resp.read()
+    if resp.status != 200:
+        print "Response status: %s\tResponse reason: %s" % (resp.status, resp.reason)
+        if hasattr(resp.msg, "x-error-detail"):
+            print "Error message: %s" % resp.msg["x-error-detail"]
+            sys.exit(1)
+    data = json.loads(data)
+    requestName = data['result'][0]['request']
+    print "  Request '%s' successfully created." % requestName
+    return requestName
 
 
 def approveRequest(workflow):
-    params = {"requestName": workflow, "status": "assignment-approved"}
+    print "Approving request..."
+    encodedParams = json.dumps({"RequestStatus": "assignment-approved"})
+    headers = {"Content-type": "application/json",
+               "Accept": "application/json"}
 
-    encodedParams = urllib.urlencode(params)
-    headers  =  {"Content-type": "application/x-www-form-urlencoded",
-                 "Accept": "text/plain"}
-
-    conn  =  httplib.HTTPSConnection(url,
-                                     cert_file = os.getenv('X509_USER_PROXY'),
-                                     key_file = os.getenv('X509_USER_PROXY'))
-    conn.request("PUT",  "/reqmgr/reqMgr/request", encodedParams, headers)
-    response = conn.getresponse()
-    if response.status != 200:
-        print 'could not approve request with following parameters:'
-        for item in params.keys():
-            print item + ": " + str(params[item])
-        print 'Response from http call:'
-        print 'Status:',response.status,'Reason:',response.reason
-        print 'Explanation:'
-        data = response.read()
-        print data
-        print "Exiting!"
-        sys.exit(1)
+    conn = httplib.HTTPSConnection(url, cert_file=os.getenv('X509_USER_PROXY'), key_file=os.getenv('X509_USER_PROXY'))
+    conn.request("PUT", "/reqmgr2/data/request/%s" % workflow, encodedParams, headers)
+    resp = conn.getresponse()
+    data = resp.read()
+    if resp.status != 200:
+        print "Response status: %s\tResponse reason: %s" % (resp.status, resp.reason)
+        if hasattr(resp.msg, "x-error-detail"):
+            print "Error message: %s" % resp.msg["x-error-detail"]
+            sys.exit(2)
     conn.close()
+    print "  Request successfully approved!"
     return
 
 
 if __name__ == '__main__':
     main()
-
