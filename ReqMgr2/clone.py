263b95e162cdbcca15231ae94e8b7fb1da6fea12
amaltaro <alan.malta@cern.ch>
2017-06-12 11:17:08 +0200

    Handle dict via command line; code cleanup

--- ReqMgr2/clone.py
+++ ReqMgr2/clone.py
@@ -1,70 +1,50 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
+import httplib
+import json
 import os
 import sys
-import urllib
-import httplib
-import re
-import json
-from pprint import pprint
-from copy import deepcopy
-from urllib import urlencode
-from urlparse import urljoin
+
+# url = "cmsweb.cern.ch"
+url = "cmsweb-testbed.cern.ch"
 
 
-#url = "cmsweb.cern.ch"
-#url = "cmsweb-testbed.cern.ch"
-url = "alan-cloud1.cern.ch"
+# url = "alan-cloud2.cern.ch"
 
 
-def getStepChainArgs():
-    "DO_NOT_USE: Hack method"
-    schema = {"Memory": 2222,
-              "ScramArch": "slc6_amd64_gcc481",
-              "Step1": {"BlockWhitelist": [], "Campaign": "TEST-Alan-Override"},
-              "Step3": {"LumisPerJob": 5}}
-    return schema
-
-def getTaskChainArgs():
-    "DO_NOT_USE: Hack method"
-    schema = {"Memory": 2444,
-              "TimePerEvent": 2.5,
-              "Task1": {"TransientOutputModules": [], "LumiList": {"202205": [[1,20], [101, 110]]}},
-              "Task2": {"LumisPerJob": 2}}
-    return schema
-
-def createClone(originalRequest, schema={}):
+def createClone(originalRequest, schema=None):
+    schema = schema or {}
     headers = {"Content-type": "application/json",
                "Accept": "application/json"}
-    #schema = getStepChainArgs()
-    #schema = getTaskChainArgs()
 
     encodedParams = json.dumps(schema)
-    conn  = httplib.HTTPSConnection(url, cert_file = os.getenv('X509_USER_PROXY'), key_file = os.getenv('X509_USER_PROXY'))
-    print "Cloning request '%s' with the following override settings: %s" % (originalRequest, schema)
+    conn = httplib.HTTPSConnection(url, cert_file=os.getenv('X509_USER_PROXY'), key_file=os.getenv('X509_USER_PROXY'))
+    print("Cloning request '%s' with the following override settings: %s" % (originalRequest, schema))
     conn.request("POST", "/reqmgr2/data/request/clone/%s" % originalRequest, encodedParams, headers)
     resp = conn.getresponse()
     data = resp.read()
     if resp.status != 200:
-        print "Response status: %s\tResponse reason: %s" % (resp.status, resp.reason)
-        print "Error message: %s" % resp.msg.getheader('X-Error-Detail')
+        print("Response status: %s\tResponse reason: %s" % (resp.status, resp.reason))
+        print("Error message: %s" % resp.msg.getheader('X-Error-Detail'))
         sys.exit(1)
 
-    #print data
     data = json.loads(data)
     requestName = data['result'][0]['request']
-    print "Clone: %s" % requestName
+    print("Clone: %s" % requestName)
     return requestName
 
 
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usage:"
-        print "python clone.y WORKFLOW_NAME"
+    if len(sys.argv) > 3 or len(sys.argv) < 2:
+        print("Usage: python clone.y WORKFLOW_NAME OVERRIDE_DICT")
+        print("  e.g.: python clone.py pdmvserv_task_HIG-PhaseIIFall16LHEGS82-00018__v1_T_170228_170033_676 '{\"key\": \"alan\"}'")
         sys.exit(0)
 
     workflow = sys.argv[1]
-    newWorkflow=createClone(workflow)
+    override = {}
+    if len(sys.argv) == 3:
+        override = json.loads(sys.argv[2])
+    newWorkflow = createClone(workflow, override)
 
     sys.exit(0)
