171f9b71234e14126b3072268ab67b791b851e33
amaltaro <alan.malta@cern.ch>
2018-08-08 14:48:28 +0200

    Count number of rows/files/files at CERN for each fileset

--- checkACDCCollections.py
+++ checkACDCCollections.py
@@ -6,17 +6,19 @@ of failures for a given workflow in a per-task basis.
 """
 
 import sys
-from pprint import pprint
 from optparse import OptionParser
+from pprint import pprint
+
 from WMCore.Database.CMSCouch import Database
+
 
 def main():
     """
     _main_
     """
     usage = "Usage: python %prog -w workflow"
-    parser = OptionParser(usage = usage)
-    parser.add_option('-w', '--workflow', help = 'Workflow name in ReqMgr', dest = 'wf')
+    parser = OptionParser(usage=usage)
+    parser.add_option('-w', '--workflow', help='Workflow name in ReqMgr', dest='wf')
     (options, args) = parser.parse_args()
     if not options.wf:
         parser.error('You must provide a workflow name')
@@ -27,15 +29,22 @@ def main():
     failures = {}
     svc = Database(database, couchUrl)
 
-    result = svc.loadView("ACDC", "byCollectionName", {'key' : options.wf, 'include_docs' : True, 'reduce' : False})
+    result = svc.loadView("ACDC", "byCollectionName", {'key': options.wf, 'include_docs': True, 'reduce': False})
     print "Found %i failures/rows in total." % len(result["rows"])
     for entry in result["rows"]:
-        if entry['doc']['fileset_name'] in failures:
-            failures[entry['doc']['fileset_name']] += 1
-        else:
-            failures[entry['doc']['fileset_name']] = 1
+        fsetName = entry['doc']['fileset_name']
+        failures.setdefault(fsetName, {'jobs': 0, 'files': 0, 'filesAtCERN': 0})
+
+        failures[fsetName]['jobs'] += 1
+        failures[fsetName]['files'] += len(entry['doc']['files'])
+
+        for f, v in entry['doc']['files'].iteritems():
+            if "T2_CH_CERN" in v['locations']:
+                failures[fsetName]['filesAtCERN'] += 1
+
     pprint(failures)
     print "\nDone!"
+
 
 if __name__ == "__main__":
     sys.exit(main())
