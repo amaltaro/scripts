aa3540bc18eb4178bee54dbd8295f71afa65ff50
Alan Malta Rodrigues <alan.malta@cern.ch>
2023-09-18 17:08:54 -0400

    Update setrequeststatus to python3

--- ReqMgr2/setrequeststatus.py
+++ ReqMgr2/setrequeststatus.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
-import sys
-import os
-import re
+"""
+This script can be used to update the status of a request in ReqMgr2.
+"""
+from __future__ import print_function, division
+
 import json
-import httplib
+import os
+import sys
+import http.client
 
 
 def setStatus(url, workflow, newstatus):
     headers = {"Content-type": "application/json",
                "Accept": "application/json"}
 
-    print "Setting %s to %s" % (workflow, newstatus)
     if newstatus in ['closed-out', 'announced', 'aborted', 'rejected']:
         encodedParams = json.dumps({"RequestStatus": newstatus, "cascade": True})
     else:
         encodedParams = json.dumps({"RequestStatus": newstatus})
 
-    conn = httplib.HTTPSConnection(url, cert_file = os.getenv('X509_USER_PROXY'), key_file = os.getenv('X509_USER_PROXY'))
-    conn.request("PUT",  "/reqmgr2/data/request/%s" % workflow, encodedParams, headers)
+    conn = http.client.HTTPSConnection(url, cert_file=os.getenv('X509_USER_PROXY'), key_file=os.getenv('X509_USER_PROXY'))
+    conn.request("PUT", "/reqmgr2/data/request/%s" % workflow, encodedParams, headers)
     resp = conn.getresponse()
-    data = resp.read()
-    #print data
     if resp.status != 200:
-        print "Response status: %s\tResponse reason: %s" % (resp.status, resp.reason)
-        if hasattr(resp.msg, "x-error-detail"):
-            print "Error message: %s" % resp.msg["x-error-detail"]
-            sys.exit(2)
+        print("Response status: %s\tResponse reason: %s\tError-detail: %s" % (resp.status, resp.reason, resp.getheader("x-error-detail")))
+        print("  FAILED status transition for: %s" % workflow)
     else:
-        print "  OK!"
+        print("  OK!")
     conn.close()
 
 
@@ -35,34 +34,44 @@ def getStatus(url, workflow):
     headers = {"Content-type": "application/json",
                "Accept": "application/json"}
 
-    conn = httplib.HTTPSConnection(url, cert_file = os.getenv('X509_USER_PROXY'), key_file = os.getenv('X509_USER_PROXY'))
+    conn = http.client.HTTPSConnection(url, cert_file=os.getenv('X509_USER_PROXY'), key_file=os.getenv('X509_USER_PROXY'))
     urn = "/reqmgr2/data/request/%s" % workflow
-    r1=conn.request("GET", urn, headers=headers)
-    r2=conn.getresponse()
-    request = json.loads(r2.read())["result"][0]
-    return request[workflow]['RequestStatus']
+    conn.request("GET", urn, headers=headers)
+    resp = conn.getresponse()
+    if resp.status != 200:
+        print("Response status: %s\tResponse reason: %s\tError-detail: %s" % (resp.status, resp.reason, resp.msg.getheader("x-error-detail")))
+        return None
+    else:
+        resp = json.loads(resp.read())["result"][0]
+    return resp[workflow]['RequestStatus']
 
 
 def main():
 #    url = 'cmsweb.cern.ch'
-#    url = 'cmsweb-testbed.cern.ch'
-    url = 'alan-cloud1.cern.ch'
+    url = 'cmsweb-testbed.cern.ch'
+#    url = 'alancc7-cloud1.cern.ch'
+#    url = 'alancc7-cloud2.cern.ch'
+#    url = 'cmsweb-k8s-testbed.cern.ch'
+#    url = 'cmsweb-test9.cern.ch'
 
-    args=sys.argv[1:]
-    if not len(args)==2:
-        print "usage: python setrequeststatus.py <text_file_with_the_workflow_names> <newStatus>"
+    args = sys.argv[1:]
+    if not len(args) == 2:
+        print("usage: python setrequeststatus.py <text_file_with_the_workflow_names> <newStatus>")
         sys.exit(0)
     inputFile = args[0]
     newstatus = args[1]
-    f = open(inputFile, 'r')
+    with open(inputFile, 'r') as fOjb:
+        workflows = fOjb.readlines()
 
-    for line in f:
-        workflow = line.rstrip('\n')
-        print("%s" % workflow)
-        print "Set %s from %s to %s" % (workflow,getStatus(url, workflow),newstatus)
-        setStatus(url, workflow, newstatus)
-#        print "Final status is: %s"  % getStatus(url, workflow)
-    f.close
+    for wflowName in workflows:
+        wflowName = wflowName.rstrip('\n')
+        currStatus = getStatus(url, wflowName)
+        if not currStatus:
+            print("  FAILED to retrieve status for workflow: %s" % wflowName)
+            continue
+        print("Setting %s status from %s to %s" % (wflowName, currStatus, newstatus))
+        setStatus(url, wflowName, newstatus)
+
 
 if __name__ == "__main__":
     main()
